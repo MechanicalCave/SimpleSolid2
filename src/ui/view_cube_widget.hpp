79b529a0769ebc0ee3a49b6977e02b7a676695fc
MechanicalCave <mechanical.cave@gmail.com>
2026-09-23 21:16:28 +0200

    WB-01: add provider-neutral ViewCube widget

---
+++ src/ui/view_cube_widget.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <simplesolid2/viewer/document_viewport.hpp>
+
+#include <QFrame>
+
+class QPushButton;
+
+namespace simplesolid2::ui {
+
+class ViewCubeWidget final : public QFrame {
+public:
+    explicit ViewCubeWidget(
+        viewer::IDocumentViewport* viewport,
+        QWidget* parent = nullptr);
+
+    void setViewport(
+        viewer::IDocumentViewport* viewport);
+
+private:
+    QPushButton* addViewButton(
+        const QString& text,
+        const QString& tooltip,
+        viewer::StandardView view,
+        int row,
+        int column);
+
+    void applyStandardView(
+        viewer::StandardView view);
+    void toggleProjection();
+    void refreshProjectionLabel();
+    void syncEnabledState();
+
+    viewer::IDocumentViewport* viewport_{};
+    class QGridLayout* grid_{};
+    QPushButton* fit_button_{};
+    QPushButton* projection_button_{};
+};
+
+} // namespace simplesolid2::ui
