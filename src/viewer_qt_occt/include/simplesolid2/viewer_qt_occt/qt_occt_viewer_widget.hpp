bfa48a7fadf3add0a3b2cef3e5ef4324317151d5
MechanicalCave <mechanical.cave@gmail.com>
2026-09-23 20:02:07 +0200

    WB-01: implement neutral viewport control in OCCT provider

--- src/viewer_qt_occt/include/simplesolid2/viewer_qt_occt/qt_occt_viewer_widget.hpp
+++ src/viewer_qt_occt/include/simplesolid2/viewer_qt_occt/qt_occt_viewer_widget.hpp
@@ -1,22 +1,23 @@
 #pragma once
-#include <simplesolid2/viewer/camera_state.hpp>
-#include <simplesolid2/viewer/navigation.hpp>
+#include <simplesolid2/viewer/document_viewport.hpp>
 #include <QWidget>
 #include <memory>
 #include <optional>
 class QPaintEngine; class QPaintEvent; class QResizeEvent; class QShowEvent; class QMouseEvent; class QWheelEvent;
 namespace simplesolid2::viewer_qt_occt {
-class QtOcctViewerWidget final:public QWidget {
+class QtOcctViewerWidget final
+    : public QWidget,
+      public viewer::IDocumentViewport {
 public:
  explicit QtOcctViewerWidget(QWidget* parent=nullptr);
  ~QtOcctViewerWidget() override;
  QtOcctViewerWidget(const QtOcctViewerWidget&)=delete;
  QtOcctViewerWidget& operator=(const QtOcctViewerWidget&)=delete;
- [[nodiscard]] std::optional<viewer::CameraState> cameraState() const;
- [[nodiscard]] bool setCameraState(const viewer::CameraState& state);
- [[nodiscard]] bool setStandardView(viewer::StandardView view);
- [[nodiscard]] bool setProjection(viewer::CameraProjection projection);
- void fitAll(); void zoomByFactor(double factor); void panByPixels(int delta_x,int delta_y); void orbitByRadians(double horizontal_radians,double vertical_radians);
+ [[nodiscard]] std::optional<viewer::CameraState> cameraState() const override;
+ [[nodiscard]] bool setCameraState(const viewer::CameraState& state) override;
+ [[nodiscard]] bool setStandardView(viewer::StandardView view) override;
+ [[nodiscard]] bool setProjection(viewer::CameraProjection projection) override;
+ void fitAll() override; void zoomByFactor(double factor); void panByPixels(int delta_x,int delta_y); void orbitByRadians(double horizontal_radians,double vertical_radians);
 protected:
  QPaintEngine* paintEngine() const override;
  void paintEvent(QPaintEvent* event) override; void resizeEvent(QResizeEvent* event) override; void showEvent(QShowEvent* event) override;
